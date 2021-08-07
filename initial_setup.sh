397b5db8eb680f9f30c5441a5de508a2a63edee9
Arjun Kanniah <arjun.kanniah@outlook.com>
2021-08-07 13:32:00 -0700

    Adding oh my zsh

--- initial_setup.sh
+++ initial_setup.sh
@@ -11,4 +11,11 @@ echo "$SUDO_PASS" | sudo apt-get upgrade -y
 echo "$SUDO_PASS" | sudo apt install python3-pip -y
 pip3 install ansible --user
 
+git config --global core.autocrlf input
+
 ansible-galaxy install -r roles/requirements.yml
+
+echo "$SUDO_PASS" | sudo apt install zsh
+echo "0" | zsh
+chsh -s $(which zsh) && \
+    sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
