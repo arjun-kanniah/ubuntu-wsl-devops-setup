02068c0d0cdd07c95116f87d51358cafbf987d2e
Arjun Kanniah <arjun.kanniah@outlook.com>
2021-08-07 10:21:30 -0700

    Adding sudoers file edits

---
+++ initial_setup.sh
@@ -0,0 +1,12 @@
+#! /usr/bin/bash
+
+read -s -p "Enter your sudo password: " SUDO_PASS
+
+echo "$SUDO_PASS" | sudo apt update
+echo "$SUDO_PASS" | sudo apt upgrade -y
+
+echo "$SUDO_PASS" | sudo apt-get update
+echo "$SUDO_PASS" | sudo apt-get upgrade -y
+
+echo "$SUDO_PASS" | sudo apt install python3-pip -y
+pip3 install ansible --user
