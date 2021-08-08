35560e5514275e83c4470ab6f5c7cf35bec32e64
Arjun Kanniah <33810967+arjun-kanniah@users.noreply.github.com>
2021-08-08 13:31:06 -0700

    Adding oh-my-zsh theme

--- initial_setup.sh
+++ initial_setup.sh
@@ -26,4 +26,16 @@ ansible-playbook setup_ubuntu.yml --vault-password-file ~/.ansible_vault_pass
 
 echo "$SUDO_PASS" | sudo usermod -aG docker $USER
 
-source ~/.bash_alias && source ~/.bashrc
+source ~/.bashrc 
+
+source ~/.bash_alias
+
+echo "$SUDO_PASS" | sudo apt install zsh -y
+echo "0" | zsh
+echo "$SUDO_PASS" | chsh -s $(which zsh)
+echo "$SUDO_PASS" | sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+sed -i "s/robbyrussel/agnoster/" ~/.zshrc
+
+source ~/.profile
+
+source ~/.zshrc
